3f09eb4927245ab967ed7a885a2a7e7d796bba8f
Aakash@edugaon <162113252+aakashlionhouse@users.noreply.github.com>
2024-03-04 15:58:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fairbase/StudentModel.kt
@@ -0,0 +1,8 @@
+package com.example.fairbase
+
+data class StudentModel(
+    var name: String? = null,
+    var number: String? = null,
+    val email: String? = null,
+    val id: String? = null
+)
